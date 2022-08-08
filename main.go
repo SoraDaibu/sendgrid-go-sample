0b8879b78e1a884b1b47a278777fcfdbcfb8ef4e
sora_shrlock <soradaibu141221@gmail.com>
2022-08-08 17:58:49 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+// using SendGrid's Go Library
+// https://github.com/sendgrid/sendgrid-go
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+func main() {
+	from := mail.NewEmail("Example User", "test@example.com")
+	subject := "Sending with SendGrid is Fun"
+	to := mail.NewEmail("Example User", "test@example.com")
+	plainTextContent := "and easy to do anywhere, even with Go"
+	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+	}
+}
